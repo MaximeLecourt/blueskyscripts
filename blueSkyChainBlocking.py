09508d92af3107222aca4b4ce7a053bfa9871c1e
Ray Powell <ray+git@xphoniexx.net>
2024-11-14 23:08:40 -0600

    some more fixes, block list doesnt work cause moderation lists different then normal lists

--- blueSkyChainBlocking.py
+++ blueSkyChainBlocking.py
@@ -19,6 +19,20 @@ def block_user(client=None, tgtUserHandle=None, tgtUserDID=None, dryRun=False):
         except Exception as e:
             print( f'ERROR: Somethign went wrong: {e}')
 
+def add_user_to_blocklist(client=None, tgtUserHandle=None, tgtUserDID=None, listURI=None, dryRun=False):
+    # It only works for normal lists, not moderation lists
+    list_record = models.AppBskyGraphListitem.Record(
+        subject=tgtUserDID,
+        created_at=client.get_current_time_iso(),
+        list=listURI,
+    )
+    if not dryRun:
+        try:
+            client.app.bsky.graph.listitem.create(client.me.did,list_record)
+            print( f'>> Added user: {tgtUserHandle} with DID: {tgtUserDID} to block list ${listURI}' )
+        except Exception as e:
+            print( f'ERROR: Somethign went wrong: {e}')
+
 def main():
 
     #arg parse flags
@@ -27,15 +41,16 @@ def main():
         description='Blocks everyone who follows the the target user.',
     )
     parser.add_argument('--blockfollowers', dest="blockFollowersOf", help="The username of the user who you want to block all their followers of")
+    parser.add_argument('--list', dest="listURI", help="The at:/ uri to a block list.")
     parser.add_argument('--dryrun', dest="dryRun", action='store_true', help="Do not actually execute the blocks")
     args = parser.parse_args()
     blockFollowersOf = args.blockFollowersOf
     dryRun=args.dryRun
+    listURI=args.listURI
 
     #Login
     client = Client()
     profile = client.login(settings.login,settings.password)
-    myDID = profile.did
 
     #Get the followers of the tgt user
     data = client.get_profile(actor=blockFollowersOf)
@@ -58,10 +73,17 @@ def main():
         tgtUserHandle = follower.handle
         tgtUserDID = follower.did
         block_user(client=client, tgtUserHandle=tgtUserHandle, tgtUserDID=tgtUserDID, dryRun=dryRun)
-
+    
+        #Add users to the block list
+        if listURI:
+            add_user_to_blocklist(client=client, tgtUserHandle=blockFollowersOf, tgtUserDID=tgtDID, listURI=listURI, dryRun=dryRun)
 
     #Block the actual target
     block_user(client=client, tgtUserHandle=blockFollowersOf, tgtUserDID=tgtDID, dryRun=dryRun)
+
+    #Add users to the block list
+    if listURI:
+        add_user_to_blocklist(client=client, tgtUserHandle=blockFollowersOf, tgtUserDID=tgtDID, listURI=listURI, dryRun=dryRun)
 
 
 if __name__ == '__main__':
